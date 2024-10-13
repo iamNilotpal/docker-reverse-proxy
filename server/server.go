2f4608155f6a7bafade22f8d0997273090ff97d5
Nilotpal Deka <iamnilotpaldeka@gmail.com>
2024-10-13 15:26:52 +0530

    Create http server to programatically create docker container.

---
+++ server/server.go
@@ -0,0 +1,101 @@
+package server
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/api/types/image"
+	"github.com/docker/docker/api/types/network"
+	"github.com/docker/docker/client"
+	"github.com/go-chi/chi/v5"
+	v1 "github.com/opencontainers/image-spec/specs-go/v1"
+)
+
+type createContainerInput struct {
+	Image string `json:"containerImage"`
+	Tag   string `json:"imageTag"`
+}
+
+func CreateRouter(cli *client.Client) http.Handler {
+	router := chi.NewRouter()
+
+	router.Post("/create", func(w http.ResponseWriter, r *http.Request) {
+		var input createContainerInput
+		if err := decode(r, &input); err != nil {
+			Respond(w, http.StatusBadRequest, "Invalid input")
+			return
+		}
+
+		images, err := cli.ImageList(r.Context(), image.ListOptions{All: false})
+		if err != nil {
+			Respond(w, http.StatusInternalServerError, "Error")
+			return
+		}
+
+		var imageExists bool
+		var img = input.Image + ":" + input.Tag
+
+	outer:
+		for _, image := range images {
+			for _, tag := range image.RepoTags {
+				if tag == img {
+					imageExists = true
+					break outer
+				}
+			}
+		}
+
+		if !imageExists {
+			fmt.Printf("\nPulling image : %s\n", img)
+			cli.ImagePull(r.Context(), img, image.PullOptions{})
+		}
+
+		resp, err := cli.ContainerCreate(
+			r.Context(),
+			&container.Config{Tty: false, Image: img},
+			&container.HostConfig{AutoRemove: true},
+			&network.NetworkingConfig{},
+			&v1.Platform{},
+			"",
+		)
+		if err != nil {
+			Respond(w, http.StatusInternalServerError, "Error creating container.")
+			return
+		}
+
+		if err := cli.ContainerStart(r.Context(), resp.ID, container.StartOptions{}); err != nil {
+			Respond(w, http.StatusInternalServerError, "Error starting container.")
+			return
+		}
+
+		info, err := cli.ContainerInspect(r.Context(), resp.ID)
+		if err != nil {
+			Respond(w, http.StatusInternalServerError, "Error getting container.")
+			return
+		}
+
+		Respond(w, http.StatusCreated, map[string]string{"url": info.Name + ".localhost"})
+	})
+
+	return router
+}
+
+func decode(r *http.Request, val any) error {
+	decoder := json.NewDecoder(r.Body)
+	decoder.DisallowUnknownFields()
+	return decoder.Decode(val)
+}
+
+func Respond(w http.ResponseWriter, statusCode int, data any) error {
+	if statusCode == http.StatusNoContent {
+		w.WriteHeader(statusCode)
+		return nil
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+
+	return json.NewEncoder(w).Encode(data)
+}
